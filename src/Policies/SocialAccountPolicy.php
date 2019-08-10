ea67bcc7e1db6bee3cfd03423e100dcb58b4d893
Audun Rundberg <audun@netliferesearch.com>
2019-08-10 19:10:51 +0200

    methods in policy not in use

--- src/Policies/SocialAccountPolicy.php
+++ src/Policies/SocialAccountPolicy.php
@@ -24,31 +24,6 @@ class SocialAccountPolicy
     }
 
     /**
-     * Determine whether the user can create social accounts.
-     *
-     * @param User $user
-     *
-     * @return mixed
-     */
-    public function create(User $user): bool
-    {
-        return true; // All authenticated users can create social accounts
-    }
-
-    /**
-     * Determine whether the user can update the social account.
-     *
-     * @param User          $user
-     * @param SocialAccount $socialAccount
-     *
-     * @return mixed
-     */
-    public function update(User $user, SocialAccount $socialAccount): bool
-    {
-        return false; // Updating is not supported, so we return false just to be sure
-    }
-
-    /**
      * Determine whether the user can delete the social account.
      *
      * @param User          $user
@@ -59,31 +34,5 @@ class SocialAccountPolicy
     public function delete(User $user, SocialAccount $socialAccount): bool
     {
         return $socialAccount->user->is($user);
-    }
-
-    /**
-     * Determine whether the user can restore the social account.
-     *
-     * @param User          $user
-     * @param SocialAccount $socialAccount
-     *
-     * @return mixed
-     */
-    public function restore(User $user, SocialAccount $socialAccount): bool
-    {
-        return false; // Soft deleting is not supported, so we return false just to be sure
-    }
-
-    /**
-     * Determine whether the user can permanently delete the social account.
-     *
-     * @param User          $user
-     * @param SocialAccount $socialAccount
-     *
-     * @return mixed
-     */
-    public function forceDelete(User $user, SocialAccount $socialAccount): bool
-    {
-        return false; // Soft deleting is not supported, so we return false just to be sure
     }
 }
