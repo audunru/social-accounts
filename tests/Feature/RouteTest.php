b7bb90081a97c255dae4a8d639f2199113ce6e6b
Audun Rundberg <audun@netliferesearch.com>
2019-08-11 17:26:50 +0200

    set configuration in setup method instead of repeating it

--- tests/Feature/RouteTest.php
+++ tests/Feature/RouteTest.php
@@ -13,6 +13,17 @@ class RouteTest extends TestCase
     protected $provider = 'google';
     protected $prefix = 'social-accounts';
 
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        config(["services.{$this->provider}.client_id" => $this->faker->uuid]);
+        config(["services.{$this->provider}.client_secret" => $this->faker->uuid]);
+        config(["services.{$this->provider}.redirect" => $this->faker->url]);
+    }
+
     public function test_disabled_provider_has_no_route()
     {
         $this->get("/{$this->prefix}/login/twitter")
@@ -21,10 +32,6 @@ class RouteTest extends TestCase
 
     public function test_it_redirects_from_login_to_provider()
     {
-        config(["services.{$this->provider}.client_id" => $this->faker->uuid]);
-        config(["services.{$this->provider}.client_secret" => $this->faker->uuid]);
-        config(["services.{$this->provider}.redirect" => $this->faker->url]);
-
         $response = $this->get("/{$this->prefix}/login/{$this->provider}");
 
         $response->assertStatus(302);
@@ -33,10 +40,6 @@ class RouteTest extends TestCase
 
     public function test_it_adds_options_to_redirect()
     {
-        config(["services.{$this->provider}.client_id" => $this->faker->uuid]);
-        config(["services.{$this->provider}.client_secret" => $this->faker->uuid]);
-        config(["services.{$this->provider}.redirect" => $this->faker->url]);
-
         SocialAccounts::registerProviderSettings('google', 'with', ['hd' => 'seinfeld.com']);
 
         $response = $this->get("/{$this->prefix}/login/{$this->provider}");
@@ -47,10 +50,6 @@ class RouteTest extends TestCase
 
     public function test_it_adds_a_scope()
     {
-        config(["services.{$this->provider}.client_id" => $this->faker->uuid]);
-        config(["services.{$this->provider}.client_secret" => $this->faker->uuid]);
-        config(["services.{$this->provider}.redirect" => $this->faker->url]);
-
         SocialAccounts::registerProviderSettings('google', 'scopes', ['amazing-scope']);
 
         $response = $this->get("/{$this->prefix}/login/{$this->provider}");
@@ -61,10 +60,6 @@ class RouteTest extends TestCase
 
     public function test_it_overwrites_scopes()
     {
-        config(["services.{$this->provider}.client_id" => $this->faker->uuid]);
-        config(["services.{$this->provider}.client_secret" => $this->faker->uuid]);
-        config(["services.{$this->provider}.redirect" => $this->faker->url]);
-
         SocialAccounts::registerProviderSettings('google', 'setScopes', ['just-this-scope']);
 
         $response = $this->get("/{$this->prefix}/login/{$this->provider}");
