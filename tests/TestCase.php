5153b4b7685ef1e1acbc5e17ae919ab7f447f385
Audun Rundberg <audun@rundberg.no>
2020-01-22 22:18:01 +0100

    set app.debug to a boolean value

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -23,6 +23,7 @@ abstract class TestCase extends BaseTestCase
 
     protected function getEnvironmentSetUp($app)
     {
+        $app['config']->set('app.debug', 'true' === env('APP_DEBUG'));
         $app['config']->set('app.key', substr(str_shuffle(str_repeat('0123456789abcdefghijklmnopqrstuvwxyz', 5)), 0, 32));
         $app->register(SocialiteServiceProvider::class);
     }
