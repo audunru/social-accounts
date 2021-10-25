26961cddfb0258b9695e8f6606ed784523e00fbd
Audun Rundberg <audun.rundberg@netlife.com>
2021-10-25 08:07:26 +0200

    test: configure api guard after it was removed from Laravel

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -27,6 +27,11 @@ abstract class TestCase extends BaseTestCase
     {
         $app['config']->set('app.debug', 'true' === env('APP_DEBUG'));
         $app['config']->set('app.key', substr(str_shuffle(str_repeat('0123456789abcdefghijklmnopqrstuvwxyz', 5)), 0, 32));
+        $app['config']->set('auth.guards.api', [
+            'driver'   => 'token',
+            'provider' => 'users',
+            'hash'     => false,
+        ]);
         $app->register(SocialiteServiceProvider::class);
     }
 
